17edec985eb1435530c3f609b17714bece266e36
heheeksdee0917 <sailor.moon_97@hotmail.com>
2026-06-11 14:05:06 +0800

    CMD fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
     include: ['lenis'],
   },
-});
+})
